a211044c98a34aeab5fe89fe9bcd1cc8c95c65d0
Ricardo Cabral <rramoscabral@gmail.com>
2015-07-12 20:33:28 +0100

    Create PSWindowsUpdate_RC_Start-Update.ps1

---
+++ Windows_Generic/Windows_Update/PSWindowsUpdate_RC_Start-Update.ps1
@@ -0,0 +1,19 @@
+﻿Set-ExecutionPolicy RemoteSigned
+
+
+<# The module can be installed manualy and extract in two places:
+
+    %USERPROFILE%\Documents\WindowsPowerShell\Modules
+    %WINDIR%\System32\WindowsPowerShell\v1.0\Modules 
+#> 
+
+# cd 'C:\temp\Powershell\Windows_Update\PSWindowsUpdate\'
+# dir function:add\*
+
+gcm -Module pswindows*
+#Import-Module PSWindowsUpdate
+#Get-WUInstall -MicrosoftUpdate -IgnoreUserInput -WhatIf -Verbose
+#Get-WUInstall –MicrosoftUpdate –ListOnly
+
+Update-WUModule
+Get-WUList
