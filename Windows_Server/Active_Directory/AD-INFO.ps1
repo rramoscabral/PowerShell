4667e24f0b3fe0e4e1f1dc7f2084340903a4096d
Ricardo Cabral <rramoscabral@gmail.com>
2015-07-12 20:36:07 +0100

    Create AD-INFO.ps1

---
+++ Windows_Server/Active_Directory/AD-INFO.ps1
@@ -0,0 +1,17 @@
+﻿# Check if the ActiveDirectory module is Loaded
+Get-Module -Name ActiveDirevtory
+
+# Check if the ActiveDirectory module is available
+Get-Module -Name ActiveDirectory -ListAvailable
+
+# Import the ActiveDirectory module
+Import-Module -Name ActiveDirectory
+
+# Find Cmdlets in the ActiveDirectory related to OrganizationalUnit
+Get-Command -Module ActiveDirectory -Name *OrganizationalUnit*
+
+# Get Organizational Unit objects
+Get-ADObject -Filter { ObjectClass -eq 'organizationalunit' }
+
+# Get Organizational Unit objects
+Get-ADObject -Filter { ObjectClass -eq 'organizationalunit' } -Properties CanonicalName | Select-Object -Property CanonicalName
